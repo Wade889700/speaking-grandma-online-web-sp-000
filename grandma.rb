527d02079bc46dff6e2d8b3d78b82ab39ae374e9
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-16 06:50:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -19,9 +19,7 @@ def speak_to_grandma(word)
       else
         return "HUH?! SPEAK UP, SONNY!"
       end
-  elsif word == word.upcase
-    return "NO, NOT SINCE 1938!"
   else
-    return nil
+    return "NO, NOT SINCE 1938!"
   end
 end
